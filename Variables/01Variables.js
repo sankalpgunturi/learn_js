184209a14d10e9328c00bcd14f19a999ba553929
sankalpgunturi <sgunturi@protonmail.com>
2022-05-21 22:58:33 +0530

    Lesson 3

---
+++ Variables/01Variables.js
@@ -0,0 +1,24 @@
+var fullName = "Sankalp Gunturi";
+
+// How to name variables
+
+// fullName
+// full-name
+// FullName
+
+var courseName = "React JS Bootcamp";
+
+var isLoggedIn = false;
+
+var loggedCount = 34;
+
+// console.log(loggedCount);
+// console.log("loggedCount");
+
+var paymentMode;
+console.log(paymentMode);
+
+
+
+paymentMode = "Credit Card";
+console.log(paymentMode);
