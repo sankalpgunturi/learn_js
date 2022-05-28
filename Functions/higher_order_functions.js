07d1f6e9eb18537f0b433587301980f858a6e92a
sankalpgunturi <sgunturi@protonmail.com>
2022-05-28 09:39:37 +0530

    higher order functions

---
+++ Functions/higher_order_functions.js
@@ -0,0 +1,17 @@
+radius = [3, 1, 4, 5];
+
+function area(radius) {
+    return Math.PI * radius * radius;
+}
+
+function diameter(radius) {
+    return 2 * radius;
+}
+
+function circumference(radius) {
+    return 2 * Math.PI * radius; 
+}
+
+const calculate = function (array, logic) {
+    
+}
