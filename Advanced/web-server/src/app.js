a347a0198c81d8a1ca2b2f0aaee73615cc4e3f00
sankalpgunturi <sgunturi@protonmail.com>
2022-07-08 18:02:25 +0530

    Calling geocode and adding some addendum npm modules

--- Advanced/web-server/src/app.js
+++ Advanced/web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const geoCode = require("../utils/geocode");
+const forecast = require("../utils/forecast");
 
 const app = express();
 
@@ -30,11 +32,27 @@ app.get("/weather", (req, res) => {
       error: "You must provide address!",
     });
   }
-  res.send({
-    forecast: "It is snowing",
-    location: "Philadelphia",
-    address: req.query.address,
-  });
+
+  geoCode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
 });
 
 app.get("/products", (req, res) => {
