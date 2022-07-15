c6bd87dfcd8295a1934868694b4d9869c2f4212d
sankalpgunturi <sgunturi@protonmail.com>
2022-07-15 15:12:04 +0530

    Understanding ObjectId()

--- Advanced/task-manager/mongodb.js
+++ Advanced/task-manager/mongodb.js
@@ -1,10 +1,17 @@
 // CRUD creates read update delete
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
+// const ObjectId = mongodb.ObjectId;
+
+const { MongoClient, ObjectId } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
+
+const id = new ObjectId();
+console.log(id.id.length);
+console.log(id.toHexString().length);
 
 MongoClient.connect(
   connectionURL,
@@ -16,19 +23,20 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection("users").insertOne(
-    //   {
-    //     name: "Rambo",
-    //     age: 54,
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert user!");
-    //     }
+    db.collection("users").insertOne(
+      {
+        _id: id,
+        name: "Vikram",
+        age: 69,
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert user!");
+        }
 
-    //     console.log(result.insertedId);
-    //   }
-    // );
+        console.log(result.insertedId);
+      }
+    );
 
     // db.collection("users").insertMany(
     //   [
@@ -50,26 +58,26 @@ MongoClient.connect(
     //   }
     // );
 
-    db.collection("tasks").insertMany(
-      [
-        {
-          description: "To buy clothes",
-          completed: true,
-        },
-        {
-          description: "To check out laundry",
-          completed: false,
-        },
-        {
-          description: "To go to the gym",
-          completed: false,
-        },
-      ],
-      (error, result) => {
-        if (error) {
-          return console.log("Unable to insert documents!");
-        }
-      }
-    );
+    // db.collection("tasks").insertMany(
+    //   [
+    //     {
+    //       description: "To buy clothes",
+    //       completed: true,
+    //     },
+    //     {
+    //       description: "To check out laundry",
+    //       completed: false,
+    //     },
+    //     {
+    //       description: "To go to the gym",
+    //       completed: false,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert documents!");
+    //     }
+    //   }
+    // );
   }
 );
