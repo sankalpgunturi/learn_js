3b7dddae442d2cad486322b6c1025ada9c4dad2e
sankalpgunturi <sgunturi@protonmail.com>
2022-07-16 18:17:36 +0530

    Understanding async-await Vs promise-chaining

--- Advanced/task-manager/playground/promise-chaining-2.js
+++ Advanced/task-manager/playground/promise-chaining-2.js
@@ -1,14 +1,25 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("62d285cd73425f60a909015a")
-  .then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
+// Task.findByIdAndDelete("62d285cd73425f60a909015a")
+//   .then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const deleteTaskAndCount = async (id) => {
+  await Task.findByIdAndDelete(id);
+  return await Task.countDocuments({ completed: false });
+};
+
+deleteTaskAndCount("62d2b2579fb8e115dc92dab3")
   .then((result) => {
-    console.log(result);
+    console.log("Number of tasks which are still incomplete: " + result);
   })
-  .catch((error) => {
-    console.log(error);
-  });
+  .catch((e) => console.log(e));
