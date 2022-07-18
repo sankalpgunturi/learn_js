0a3a94f9531600c4664a4ef38a43bb437ce95d93
sankalpgunturi <sgunturi@protonmail.com>
2022-07-18 13:42:58 +0530

    Adding support for file uploads (user avatars)

--- Advanced/task-manager/src/routers/user.js
+++ Advanced/task-manager/src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
+const multer = require("multer");
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
@@ -81,6 +82,14 @@ router.delete("/users/me", auth, async (req, res) => {
   } catch (error) {
     res.status(500).send();
   }
+});
+
+const upload = multer({
+  dest: "avatars",
+});
+
+router.post("/users/me/avatar", upload.single("avatar"), async (req, res) => {
+  res.send();
 });
 
 module.exports = router;
