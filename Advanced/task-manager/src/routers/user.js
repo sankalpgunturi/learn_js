872858025025f0e83e4d7535cde7e321bd5761e4
sankalpgunturi <sgunturi@protonmail.com>
2022-07-18 15:08:37 +0530

    Auto-cropping and image formatting

--- Advanced/task-manager/src/routers/user.js
+++ Advanced/task-manager/src/routers/user.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const multer = require("multer");
+const sharp = require("sharp");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
-const multer = require("multer");
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
@@ -102,7 +103,11 @@ router.post(
   auth,
   upload.single("avatar"),
   async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
   },
@@ -125,7 +130,7 @@ router.get("/users/:id/avatar", async (req, res) => {
       throw new Error();
     }
 
-    res.set("Content-Type", "image/jpg");
+    res.set("Content-Type", "image/png");
     res.send(user.avatar);
   } catch (error) {
     res.status(404).send();
