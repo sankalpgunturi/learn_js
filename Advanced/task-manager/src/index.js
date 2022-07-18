85571e68f86637e5f184bae96f78d08a75841ead
sankalpgunturi <sgunturi@protonmail.com>
2022-07-18 12:13:08 +0530

    Cascade Delete Tasks

--- Advanced/task-manager/src/index.js
+++ Advanced/task-manager/src/index.js
@@ -13,18 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
-const Task = require("./models/task");
-const User = require("./models/user");
-
-const main = async () => {
-  // const task = await Task.findById("62d409ab967d3f16900052da");
-  // await task.populate("owner").execPopulate();
-  // console.log(task.owner);
-
-  const user = await User.findById("62d4f463f9f2c91d85398951");
-  await user.populate("tasks").execPopulate();
-  console.log(user.tasks);
-};
-
-main();
