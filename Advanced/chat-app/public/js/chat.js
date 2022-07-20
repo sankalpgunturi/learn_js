308ebf9f20045434f70ad039eeeb1b3779024dea
sankalpgunturi <sgunturi@protonmail.com>
2022-07-20 10:09:51 +0530

    Sending messages between server and client using socket

--- Advanced/chat-app/public/js/chat.js
+++ Advanced/chat-app/public/js/chat.js
@@ -1,1 +1,13 @@
-io();
+const socket = io();
+
+socket.on("message", (welcome) => {
+  console.log(welcome);
+});
+
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = document.querySelector("input").value;
+
+  socket.emit("sendMessage", message);
+});
