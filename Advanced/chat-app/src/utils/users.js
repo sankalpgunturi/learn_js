1747b2e6c42df82b8f833af94eed2668f20926ce
sankalpgunturi <sgunturi@protonmail.com>
2022-07-20 14:46:04 +0530

    Storing users: adding and removing

---
+++ Advanced/chat-app/src/utils/users.js
@@ -0,0 +1,54 @@
+const users = [];
+
+// Add User
+const addUser = ({ id, username, room }) => {
+  // Clean the data
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  // Validate the data
+  if (!username || !room) {
+    return {
+      error: "Username and Room are required!",
+    };
+  }
+
+  // Check for existing user
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username;
+  });
+
+  // Validate username
+  if (existingUser) {
+    return {
+      error: "Username is in use!",
+    };
+  }
+
+  // Store user
+  const user = { id, username, room };
+  users.push(user);
+  return { user };
+};
+
+// Remove User
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+addUser({
+  id: 22,
+  username: "Sankalp",
+  room: "South Philly",
+});
+
+const removedUser = removeUser(22);
+console.log(removedUser);
+console.log(users);
+
+// Get User
+
+// Get Users in Room
